0d4b55761c0ef5a61a0991e8790b5a2df591ab92
lukicusi <lukicusi@gmail.com>
2024-05-31 21:01:07 -0500

    comments practice

--- how-to-write-code/Comments.js
+++ how-to-write-code/Comments.js
@@ -4,6 +4,13 @@
 
    console.log("Hello, World!"); // Comments do not have to start at the beginning of a line.
 
+   // adding a short comment here
+
+   /* here is another
+   longer paragraph looking
+   type comment */
+
+   console.log("Testing the print function");
    /* Here is how
    to have
    multi-line
