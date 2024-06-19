407f22a4d13999b68fa42311036b75e84eda33d9
lukicusi <lukicusi@gmail.com>
2024-06-18 20:45:21 -0500

    completed ch7 code snippets

--- stringing-characters-together/code-snippets/bracket-notation.js
+++ stringing-characters-together/code-snippets/bracket-notation.js
@@ -2,3 +2,6 @@ let jsCreator = "Brendan Eich";
 
 console.log(jsCreator[-1]);
 console.log(jsCreator[42]);
+console.log(jsCreator[0]);
+console.log(jsCreator[1]);
+console.log(jsCreator[7]);
--- stringing-characters-together/code-snippets/mad-libs.js
+++ stringing-characters-together/code-snippets/mad-libs.js
@@ -1,7 +1,7 @@
-let pluralNoun = ;
-let name = ;
-let verb = ;
-let adjective = ;
-let color = ;
+let pluralNoun = "chairs" ;
+let name = "Johnny";
+let verb = "move" ;
+let adjective = "happy" ;
+let color = "yellow" ;
 
 console.log("JavaScript provides a "+ color +" collection of tools — including " + adjective + " syntax and " + pluralNoun + " — that allows "+ name +" to "+ verb +" with strings.")
