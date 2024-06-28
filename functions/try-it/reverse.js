9b13ba2bcb9558c5643748793b257c5e4cc9fabb
lukicusi <lukicusi@gmail.com>
2024-06-27 21:31:16 -0500

    completed examples try-it for ch 10 functions

--- functions/try-it/reverse.js
+++ functions/try-it/reverse.js
@@ -3,3 +3,10 @@ function reverse(str) {
    let reversedLettersArray = lettersArray.reverse();
    return reversedLettersArray.join('');
 }
+
+// LUCAS- directions are: Use method chaining to reduce the reverse function to a single line.
+//I think below is the correct chainging method. 
+//((ripped from textbook after reverse.js before isPalindrome.js in composing functions section))
+function reverse(str) {
+   return str.split('').reverse().join('');
+}
--- functions/try-it/sayHello.js
+++ functions/try-it/sayHello.js
@@ -1,3 +1,5 @@
 function sayHello() {
    console.log("Hello, World!");
 }
+//Lucas- i added code here so it would print!
+sayHello();
