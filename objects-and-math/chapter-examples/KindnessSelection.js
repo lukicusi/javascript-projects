a36f13fdc7c2e4d0b8e5d29c3a957f9cad242d69
lukicusi <lukicusi@gmail.com>
2024-07-05 18:27:45 -0500

    ch 12 objects math chapter examples completed

--- objects-and-math/chapter-examples/KindnessSelection.js
+++ objects-and-math/chapter-examples/KindnessSelection.js
@@ -11,7 +11,31 @@ function randomSelection(arr){
    console.log(randomSelection(happiness));
  }
  
+
  //Experiment with the code above. Try to:
  //a) Print 3 random selections from each array.
+ for (i=0; i < 3; i++){
+  console.log(randomSelection(happiness));
+  console.log(randomSelection(words));
+}
+  
+
  //b) Have the code randomly pick one array, and then print 2 random items from it.
+ 
+ let arrays = [[happiness], [words]];
+ for (i=0; i < 1; i++){
+  let chosen = (randomSelection(arrays));
+  console.log(chosen);
+    for (i=0; i < 2; i++){
+      console.log(randomSelection(chosen[0]));
+    }
+ }
+
+
  //c) Create a new array, then fill it with one random item from words and happiness. Print the new array.
+ let newArray= []
+ for (i=0; i<1; i++) {
+  console.log(newArray.push(randomSelection(happiness)));
+  console.log(newArray.push(randomSelection(words)));
+ }
+ console.log(newArray);
