94590edaf7c9f875ce85b2b3c752865eee4b47d3
lukicusi <lukicusi@gmail.com>
2024-07-18 16:56:40 -0500

    ch 17 exercises complete

--- exceptions/exercises/test-student-labs.js
+++ exceptions/exercises/test-student-labs.js
@@ -1,9 +1,13 @@
 function gradeLabs(labs) {
   for (let i=0; i < labs.length; i++) {
     let lab = labs[i];
-    let result = lab.runLab(3);
-    console.log(`${lab.student} code worked: ${result === 27}`);
+    try {
+      let result = lab.runLab(3);
+      console.log(`${lab.student} code worked: ${result === 27}`);
+    } catch (err) {
+      console.log("Error thrown.");
   }
+ }
 }
 
 let studentLabs = [
@@ -22,3 +26,27 @@ let studentLabs = [
 ];
 
 gradeLabs(studentLabs);
+
+//LUCASSS i added all my new code beolw here, should i move this first gradeLabs call to bottom of code?
+let studentLabs2 = [
+  {
+      student: 'Blake',
+      myCode: function (num) {
+        return Math.pow(num,num);
+      }
+  },
+  {
+      student: 'Jessica',
+      runLab: function (num) {
+        return Math.pow(num, num);
+      }
+  },
+  {
+      student: 'Mya',
+      runLab: function (num) {
+        return num * num;
+      }
+  }
+];
+
+gradeLabs(studentLabs2);
