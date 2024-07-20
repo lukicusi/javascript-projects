e18a260b2461e2f9b53d023b230bd02436067776
lukicusi <lukicusi@gmail.com>
2024-07-19 21:50:45 -0500

    pushing ch 11 ch-examples passing-functions-as-arguments

--- more-on-functions/chapter-examples/passing-functions-as-arguments/halve-each-number.js
+++ more-on-functions/chapter-examples/passing-functions-as-arguments/halve-each-number.js
@@ -1,7 +1,10 @@
 let nums = [3.14, 42, 4811];
+let takeHalf = function (n) {
+    return n/2;
+};
 
 // TODO: Write a mapping function
 // and pass it to .map()
-let halved = nums.map();
+let halved = nums.map(takeHalf);
 
 console.log(halved);
--- more-on-functions/chapter-examples/passing-functions-as-arguments/map-array-of-strings.js
+++ more-on-functions/chapter-examples/passing-functions-as-arguments/map-array-of-strings.js
@@ -1,7 +1,9 @@
 let names = ["Chris", "Jim", "Sally", "Blake", "Paul", "John", "Courtney", "Carly"];
-
+let takeFirstI = function (names) {
+return (names[0][0]);
+};
 // TODO: Write a mapping function
 // and pass it to .map()
-let firstInitials = names.map();
+let firstInitials = names.map(takeFirstI);
 
 console.log(firstInitials);
